877d61d2f080a0b6f6c37174af0810f003660778
T1mofi <sikorskitimofei@gmail.com>
2019-03-06 10:23:22 +0300

    full refactor

--- Lab3_Sum_array_str(5X6).asm
+++ Lab3_Sum_array_str(5X6).asm
@@ -98,7 +98,7 @@ INPUT_ARRAY:
         mov   CL, columns       
                             
         loop2:                    
-            call  get_number                        ; get_number() - result in AX
+            call  get_number                        ; AX get_number() 
             mov   array[SI], AX                     ;
             add   SI, 2                             ;
 
@@ -120,18 +120,18 @@ SUM:
     xor   SI, SI             
     xor   DI, DI             
 
-    sum_loop1:                                      ; do{
+    sum_loop1:                                     
         push  CX                 
         xor   CX, CX             
         mov   CL, columns        
                             
-        sum_loop2:                                  ;   do{
-            mov   AX, array[SI]                     ;       AX = array[SI]
-            add   result[DI], AX                    ;       result[DI] += AX
-            jo    sum_error                         ;       ? OF==1 goto sum_error (overflow flag) - set sum in row = 0
+        sum_loop2:                                     
+            mov   AX, array[SI]                     ; AX = array[SI]
+            add   result[DI], AX                    ; result[DI] += AX
+            jo    sum_error                         ; ? OF==1 (overflow flag) - set sum in row = 0
             add   SI, 2                 
         loop  sum_loop2
-                                                    ;   }while(--CX!=0)
+                                                    
         to_next_row:            
         pop   CX                 
         add   DI, 2
@@ -215,105 +215,110 @@ get_number proc
         jmp   num_inp            
                              
 
-    converting:                                     ; form string to int                          
-        xor   CX, CX                                ; CX = 0
-        mov   CL, number[1]                         ; CL = len of string with number
-        mov   SI, CX                                ; SI = CX
-        inc   SI                                    ; ++SI - set on last sumbol of sting
-        xor   BX, BX                                ; BX = 0 - stores the result                                
+    converting:                                     ; convert string to int                          
+        xor   CX, CX                                
+        mov   CL, number[1]                         ; real string len
+        
+        mov   SI, CX                                ; set on last sumbol of sting                           
+        inc   SI                                    ; 
+        
+        xor   BX, BX                                                                
         mov   DX, 1                                 ; DX = 1 - discharge multiplier
-        push  DX                                    ; push DX to stack
-        xor   AX, AX                                ; AX = 0 - value of discharge
+        push  DX                                    
+        xor   AX, AX                                ; value of discharge
                                    
-        converting_loop:              ; do{
-            mov   AL, number[SI]      ;      AL = number[SI]
-            cmp   AL, '-'             ;      
-            je    to_neg              ;
-            and   AX, 0Fh             ;      char to int(-48)
+        converting_loop:              
+            mov   AL, number[SI]      
+            cmp   AL, '-'                   
+                je    to_neg
+                              
+            and   AX, 0Fh                           ; char to int(-48)
     
-            mul   DX                  ;      AX = AX*DX, DX=0
-            pop   DX                  ;      pop DX from stack
-            jo    overflow            ;      ? OF==1 goto overflow (overflow flag)
+            mul   DX                                ; AX *= DX, DX=0
+            pop   DX                  
+                jo    overflow                      ; ? OF==1 goto overflow (overflow flag)
    
-            add   BX, AX              ;      BX +=AX
-            js    overflow            ;      ? SF==1 goto overflow (sign flag)
-            jo    overflow            ;      ? OF==1 goto overflow (overflow flag) 
+            add   BX, AX                            ; BX +=AX
+                js    overflow                      ; ? SF==1 goto overflow (sign flag)
+                jo    overflow                      ; ? OF==1 goto overflow (overflow flag) 
    
-            cmp   SI, 02h             ;      ? SI==02h goto end_get
-            je    end_get             ;      02h - start of number 
+            cmp   SI, 02h                           
+                je    end_get              
    
-            mov   AX, 10              ;      DX = 10
-            mul   DX                  ;      AX = AX*DX 
-            jo    overflow            ;      ? OF==1 goto overflow (overflow flag)
+            mov   AX, 10                            
+            mul   DX                                ; AX *= DX 
+                jo    overflow                      ; ? OF==1 goto overflow (overflow flag)
    
-            xchg  DX, AX              ;      AX <-> DX
-            push  DX                  ;      push DX to stack
-            dec   SI                  ;      --SI
-        loop  converting_loop     ; } while(CX--!=0)
-        jmp   end_get             ; goto end_get
-                             ;
-                             ;
-    to_neg:                      ; convert to neg
-        pop   DX                  ; pop form stack 
-        neg   BX                  ; BX = -BX conversion to reverse code
-        jo    overflow            ; ? OF==1 goto overflow (overflow flag)
-        jmp   end_get             ; goto end_get
-                             ;
+            xchg  DX, AX                            
+            push  DX                  
+            dec   SI                
+        loop  converting_loop     
+        jmp   end_get             
+                            
+    to_neg:                      
+        pop   DX                   
+        neg   BX                                    ; BX = -BX conversion to reverse code
+            jo    overflow                          ; ? OF==1 goto overflow (overflow flag)
+            jmp   end_get            
 
-    overflow:                    ; overflow
-        mov   AH, 09h             ;
-        lea   DX, msg_overfl        ;
-        int   21h                 ;
-        jmp   num_inp            ; enter again
+    overflow:                    
+        mov   AH, 09h             
+        lea   DX, msg_overfl       
+        int   21h                 
+        jmp   num_inp            
 
-    end_get:                     ;
-        mov   AX, BX              ; result to AX
-        pop   DI                  ; recovery DI, SI, BX. CX
-        pop   SI                  ;
-        pop   BX                  ;
-        pop   CX                  ;
+    end_get:                     
+        mov   AX, BX                                ; result to AX  
+        
+        pop   DI                                    ; recover DI, SI, BX. CX
+        pop   SI                                    ;
+        pop   BX                                    ;
+        pop   CX                                    ;
         ret
-                               ; return
-get_number endp          ; }  
+                              
+get_number endp           
    
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;  
  
  
-OutInt proc                  ;void output_int(AX = intNumber)
-                             ;{
-    push CX                  ; push CX to stack
-    test AX, AX              ; if(AX>=0)
-    jns  oi1                 ;  goto oi1
-                             ;
-    mov  BX, AX              ; BX = AX
-    mov  AH, 02h             ; {
-    mov  DL, '-'             ;  cout << '-'
-    int  21h                 ; }
-    mov  AX, BX              ; AX = BX
-    neg  AX                  ; AX = -AX
+OutInt proc                                         ;void output_int(AX = intNumber)
+                             
+    push CX                   
+    test AX, AX                                     ; if (AX>=0)
+        jns  oi1               
+                             
+    mov  BX, AX              
+    mov  AH, 02h             
+    mov  DL, '-'             
+    int  21h                 
+    mov  AX, BX              
+    neg  AX                                         ; AX = -AX
     
-    oi1:                         ;
-        xor  CX, CX              ; CX = 0
-        mov  BX, 10              ; BX = 10
-    oi2:                         ; do{
-        xor  DX,DX               ;  DX = 0
-        div  BX                  ;  AX /= BX, DX = AX % BX
-        push DX                  ;  push DX to stack
-        inc  CX                  ;  ++CX
-        test AX, AX              ; 
-        jnz  oi2                 ; }while(AX!=0)
-        mov  AH, 02h             ; AH = O2h - output one sumbol
-    oi3:                         ; do{
-        pop  DX                  ;  pop DX from stack 
-        add  DL, '0'             ;  DL += '0' (48) - char to int
-        int  21h                 ;  interrupt for implementation
-    loop oi3                 ; }while(CX--!=0)
+    oi1:                         
+        xor  CX, CX              
+        mov  BX, 10
+                      
+        oi2:                         
+            xor  DX,DX               
+            div  BX                                 ;  AX /= BX, DX = AX % BX
+            push DX                  
+            inc  CX                  
+            test AX, AX               
+                jnz  oi2 
+                                
+            mov  AH, 02h 
+                        
+        oi3:                         
+            pop  DX                   
+            add  DL, '0'                            ;  DL += (48) - char to int
+            int  21h                 
+        loop oi3                 
     
-    pop  CX                  ; pop CX from stack
-    ret                      ; return
-                             ;
-OutInt endp                  ;}
+    pop  CX                  
+    ret                     
+                             
+OutInt endp                  
        
    
 exit:  
@@ -337,7 +342,7 @@ end START
         ;!!!!!!!!!!!!!!!!!!!!!!!!    0             0    !!!!!!!!!!!!!!!!!!!!!!!!!!!
         ;!!!!!!!!!!!!!!!!!!!!!!!                         !!!!!!!!!!!!!!!!!!!!!!!!!!
         ;!!!!!!!!!!!!!!!!!!!!!!!!                       !!!!!!!!!!!!!!!!!!!!!!!!!!!
-        ;!!!!!!!!!!!!!!!!!!!!!!!!!       ___           !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        ;!!!!!!!!!!!!!!!!!!!!!!!!!        \___/        !!!!!!!!!!!!!!!!!!!!!!!!!!!!
         ;!!!!!!!!!!!!!!!!!!!!!!!!!!                  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         ;!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         ;!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
